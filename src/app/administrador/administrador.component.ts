b4a347cc2d1aff847c602270e277f3465b5a6aef
cristhofer24 <you@example.com>
2024-09-04 23:57:28 -0500

    CRUD

--- src/app/administrador/administrador.component.ts
+++ src/app/administrador/administrador.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-administrador',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './administrador.component.html',
   styleUrl: './administrador.component.scss'
 })
 export class AdministradorComponent {
+  isModalOpen = false;
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 
 }
