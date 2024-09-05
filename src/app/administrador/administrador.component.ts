7fcbe0468baa90da6f3fc6949b055c0f823f3ce7
Santos-de-Israel <144865373+Santos-de-Israel@users.noreply.github.com>
2024-09-04 23:53:04 -0500

    CRUD

--- src/app/administrador/administrador.component.ts
+++ src/app/administrador/administrador.component.ts
@@ -1,12 +1,37 @@
+/* eslint-disable no-irregular-whitespace */
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-administrador',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './administrador.component.html',
   styleUrl: './administrador.component.scss'
 })
 export class AdministradorComponent {
+  isModalOpen = false;
+  isModalOpen1 = false;
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+  openModal1() {
+    this.isModalOpen1 = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.isModalOpen1 = false;
+  }
+
+// ngAfterViewInit(): void {
+//   // Ejecuta el clic en el botón una vez que el DOM está completamente cargado
+//   const readProductButton = document.getElementById('readProductButton') as HTMLButtonElement;
+  
+//   if (readProductButton) {
+//     readProductButton.click();
+//   }
+// }
 
 }
