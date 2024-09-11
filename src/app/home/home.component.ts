da209f171755e50d2c8a220c530112ed84e02c40
ItsqmetEstudiantes <hancellsud@gmail.com>
2024-09-11 09:41:20 -0500

    carrusel corregido

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 
+
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -8,6 +10,22 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
+
+
 export class HomeComponent {
 
+  activeIndex = 0;
+  totalSlides = 3;
+
+  prevSlide(): void {
+    this.activeIndex = (this.activeIndex === 0) ? this.totalSlides - 1 : this.activeIndex - 1;
+  }
+
+  nextSlide(): void {
+    this.activeIndex = (this.activeIndex === this.totalSlides - 1) ? 0 : this.activeIndex + 1;
+  }
+
+  showSlide(index: number): void {
+    this.activeIndex = index;
+  }
 }
