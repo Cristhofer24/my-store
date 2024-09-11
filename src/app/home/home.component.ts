3db66793ebdf21cbb820372484a78e169fde69a9
ItsqmetEstudiantes <hancellsud@gmail.com>
2024-09-11 10:39:04 -0500

    toques finales de carrucel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,10 +13,15 @@ import { Component } from '@angular/core';
 
 
 export class HomeComponent {
-
+//carrusel 1
   activeIndex = 0;
   totalSlides = 3;
+  //carrusel 2
+  activeIndex1 = 0;
+  totalSlides1 = 3;
 
+  /////////////////////////////////////////////
+//carrusel 1
   prevSlide(): void {
     this.activeIndex = (this.activeIndex === 0) ? this.totalSlides - 1 : this.activeIndex - 1;
   }
@@ -27,5 +32,20 @@ export class HomeComponent {
 
   showSlide(index: number): void {
     this.activeIndex = index;
+  }
+
+  //////////////////////////////////////////////
+  //Carrucel 2
+
+  prevSlide1(): void {
+    this.activeIndex1 = (this.activeIndex1 === 0) ? this.totalSlides1 - 1 : this.activeIndex1 - 1;
+  }
+
+  nextSlide1(): void {
+    this.activeIndex1 = (this.activeIndex1 === this.totalSlides1 - 1) ? 0 : this.activeIndex1 + 1;
+  }
+
+  showSlide1(index: number): void {
+    this.activeIndex1 = index;
   }
 }
